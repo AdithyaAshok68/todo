59e1d795c7cb9fb1b8a5b892f420004883e36d67
Adithya Ashok <adithyaashok68@gmail.com>
2024-02-19 10:07:58 +0530

    todo

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [toDos, setToDos] = useState([]);
+  const [toDo, setToDo] = useState('');
+  const handleDelete=(id)=>{
+    setToDos(toDos.filter((obj)=> obj.id!==id))
+  }
+  return (
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday ?? ? </h2>
+      </div>
+      <div className="input">
+        <input onChange={(e) => setToDo(e.target.value)} type="text" placeholder="??? Add item..." />
+        <i onClick={() => setToDos([...toDos, { id: Date.now(), text: toDo, status: false }])} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        {
+          toDos.map((obj) =>
+
+            <div className="todo" key={obj.id}>
+              <div className="left">
+                <input 
+                onChange={(e)=>{
+console.log(e.target.checked);
+console.log(obj);
+setToDos(toDos.filter(obj2=>{
+  if(obj2.id===obj.id){
+    obj2.status=e.target.checked
+  }
+  return obj2
+}))
+                }
+              }
+             value={obj.status}     
+                  type="checkbox" name="" id="" />
+                <p>{obj.text}</p>
+              </div>
+              <div className="right">
+                <i className="fas fa-times"
+                onClick={()=> handleDelete(obj.id)}></i>
+              </div>
+            </div>
+
+          )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
